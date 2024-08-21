7caf47ae95a185303c6b7822d5e57fce14a53fea
williameaton <weaton@princeton.edu>
2024-08-21 16:07:18 -0400

    test suite minor update

--- specfem_mesh/tests/integration/cleanup.sh
+++ specfem_mesh/tests/integration/cleanup.sh
@@ -6,6 +6,7 @@ rm ${this_dir}/*.mod
 rm -r ${this_dir}/__pycache__
 rm ${this_dir}/ylm_plm.mod
 rm ${this_dir}/test_ylm_integration
+rm ${this_dir}/test_1_integration
 rm ${this_dir}/test_ylm_int_2.txt
 rm ${this_dir}/test_ylm_int_3.txt
 rm ${this_dir}/test_1_int.txt
